199967fef4f37a6ab25cc1aed363062444aa2144
Tunax-C <ariestina.tc@gmail.com>
2020-10-29 16:27:02 +0800

    s

--- SimpleRecursionDemos.java
+++ SimpleRecursionDemos.java
@@ -1,5 +1,5 @@
 /*
- * PowerRecurssion.java
+ * SimpleRecursionDemos.java
  * 
  * Copyright 2020 Tina Chen <ChenTina@10-72-120-211.wifi.8.s.pas.ofs.edu.sg>
  * 
@@ -22,61 +22,11 @@
  */
 
 
-public class PowerRecurssion 
-{
+public class SimpleRecursionDemos {
 	
-	public static void main (String[] args)
-	{
-		if (base == 1)
-		{
-			return 1;
-		}
-		else if(expo == 0)
-		{
-			return 1;
-		}
-		else if(expo == 1)
-		{
-			return base;
-		}
-		else
-		{
-			return base * power(base, expo-1);
-		}
-	}
-	
-	static double superPower(int base, int expo)
-	{
-		if (base == 1)
-		{
-			return 1;
-		}
-		else if (expo == 0)
-		{
-			return 1;
-		}
-		else if (expo == 1)
-		{
-			return base;
-		}
-		else if (expo < 0)
-		{
-			return 1.0 / power(base, expo * -1);
-		}
-		else
-		{
-			return base * power(base, expo-1);
-		}		
+	public static void main (String[] args) {
 		
-	}
-	
-	public static void main (String[]args)
-	{
-		System.out.println(power(2, 10));
-		System.out.println(power(0, 0));
-		System.out.println(power(1, 100));
-		System.out.println(power(99, 1));
-		System.out.println(superPower(2, -2));
+		
 	}
 }
 
