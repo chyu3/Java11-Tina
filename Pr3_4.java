df6e6ac308185cab8fbee702cd846f522f457e3e
Tunax-C <ariestina.tc@gmail.com>
2021-01-03 11:46:42 +0800

    summary

---
+++ Pr3_4.java
@@ -0,0 +1,42 @@
+/*
+ * Pr3_4.java
+ * 
+ * Copyright 2020 Tina Chen <ChenTina@10-72-120-211.wifi.8.s.pas.ofs.edu.sg>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+public class Pr3_4 {
+	
+	public static void main (String[] args) {
+		int n = IBIO.inputInt("Enter a number ");
+		int even = n % 2;
+		do 
+		{
+			even = 0;
+			int nextn = even / 2;
+
+		}
+		while (n != 0);
+		if (even == 0)
+		
+		System.out.println(n);
+	}
+}
+
