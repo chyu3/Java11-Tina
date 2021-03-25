f32b67021ed497ee4a87c9fb81f99dc002607179
Tunax-C <ariestina.tc@gmail.com>
2021-03-25 14:54:38 +0800

    Create Pr8_1.java

---
+++ Pr8_1.java
@@ -0,0 +1,49 @@
+/*
+ * Pr8_1.java
+ * 
+ * Copyright 2021 Tina Chen <ChenTina@10-72-120-211.wifi.8.s.pas.ofs.edu.sg>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+public class Pr8_1 {
+	
+	public static void main (String[] args) 
+	{
+		for (int i = 1; i < 100; i++)
+		{
+			for (int j = 1; j < 100; j++)
+			{
+				for (int k = 1; k < 100; k++)
+				{
+					if ( good(i,j) && good(j,k) && good(i,k) )
+					IBIO.output(i+" "+j+" "+k);
+				}
+			}
+		}
+	}
+	
+	static boolean good (int a, int b)
+	{
+		int x = a * b + 1;
+		int y = (int) (Math.sqrt(x) + 5);
+		return (y * y == x);
+	}
+}
+
