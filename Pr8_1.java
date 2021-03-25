afbfd9a88a40ab3ff1be6463f7e3846c94803a70
Tunax-C <ariestina.tc@gmail.com>
2021-03-25 15:46:27 +0800

    Update Pr8_1.java

--- Pr8_1.java
+++ Pr8_1.java
@@ -28,12 +28,11 @@ public class Pr8_1 {
 	{
 		for (int i = 1; i < 100; i++)
 		{
-			for (int j = 1; j < 100; j++)
+			for (int j = i; j < 100; j++)
 			{
-				for (int k = 1; k < 100; k++)
+				for (int k = j; k < 100; k++)
 				{
-					if ( good(i,j) && good(j,k) && good(i,k) )
-					IBIO.output(i+" "+j+" "+k);
+					System.out.println(i + "" + "" + j + "" + k);
 				}
 			}
 		}
