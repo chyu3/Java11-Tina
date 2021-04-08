dae55f1a940aad9cdade3c5f162b19be7a6bfef3
Tunax-C <ariestina.tc@gmail.com>
2021-04-08 14:43:35 +0800

    Create QueueDemo2.java

---
+++ HL/QueueDemo2.java
@@ -0,0 +1,22 @@
+
+public class QueueDemo2
+{
+	public static void printArray(int[] a)
+	{
+		for(int i = 0; i < a.length; i++)
+		{
+			System.out.println( i + " : " + a[i] );
+		}
+		System.out.println();
+	}
+	
+	public static void main (String[] args)
+	{
+		int[] intArray = { 1,2,3,4,5,6,7,8,9 };
+		Queue q = new Queue();
+		// copy the array 'intArray' into the queue 'q'
+		// display the resulting queue
+		
+	}
+}
+
