8b06602d8afa9a4dd1e3d45bb00c2336586569aa
Tunax-C <ariestina.tc@gmail.com>
2021-08-20 10:42:15 +0800

    s

--- Assessments/MyMockTestPower.java
+++ Assessments/MyMockTestPower.java
@@ -2,14 +2,15 @@ package Assessments;/* MockTestPower to the 'x' to the power of 'y'
 */
 
 public class MyMockTestPower {
-	
-	public static void main (String[] args) {
+
+	public static void main(String[] args)
+	{
 		int power;
 		int base;
 		int exponent;
 		double negativePower;
-		
-		IBIO.output("Tina");
+
+		IBIO.output("Marcos Drien");
 		do
 		{
 			power = 1;
@@ -19,55 +20,44 @@ public class MyMockTestPower {
 			if( base != 0 )
 			{
 				exponent = IBIO.inputInt("Enter the exponent = ");
-				IBIO.out( "\n" + base + "to the power of" + exponent + " = " );
-				
-				if( exponent < 0 );  //negative exponent
-				{
-					exponent = exponent * -1;
-					for(i = 0; i < exponent; i++)
-					{
-						power = power * base;
+				IBIO.out( "\n" + base + " to the power of " + exponent + " = " );
+
+				if( exponent < 0 )			// NEGATIVE EXPONENT
+				{	exponent = exponent * -1;
+					for(int i = 0; i < exponent; i++)
+					{	power = power * base;
 					}
 					negativePower = 1.0/ power;
 					String denominator = base + "^" + exponent;
-					int denominatorLength = denominator.length(); //how many character in the denominator of fraction
-					
-					IBIO.output("\n"); //go to next-next line
-					for(int i = 1; i <= denominatorLength/2; i++) //put spaces...
-					{
-						IBIO.out(" "); // ...before the 1
-					}
-					
-					IBIO.output("1");
-					for(int i = 1; i <= denominatorLength; i++) //put line...
-					{
-						IBIO.out("-"); //...under the "1"
-					}
-					IBIO.output(" = "); //add the "=" and go to next line
-					IBIO.output(denominator);
-					
-					denominatorLength = 0; //line 41~45: another way to count digits
-					for(int i = power; i > 0; i=i/10)
-					{
-						denominatorLength++;
-					}
-					
+					int denominatorLength = denominator.length(); // how many characters in the denominator of fraction
 					IBIO.output("\n"); // go to next-next line
 					for(int i = 1; i <= denominatorLength/2; i++) // put spaces...
 					{	IBIO.out(" "); // ...before the "1"
 					}
-					
 					IBIO.output("1");
 					for(int i = 1; i <= denominatorLength; i++) // put line...
 					{	IBIO.out("-");   // ...under the "1"
 					}
-					
+					IBIO.output(" = ");  // add the "=" and go to next line
+					IBIO.output(denominator);
+
+					denominatorLength = 0; // lines 41~45: another way to count digits
+					for(int i = power; i > 0; i=i/10)
+					{	denominatorLength++;
+					}
+					IBIO.output("\n"); // go to next-next line
+					for(int i = 1; i <= denominatorLength/2; i++) // put spaces...
+					{	IBIO.out(" "); // ...before the "1"
+					}
+					IBIO.output("1");
+					for(int i = 1; i <= denominatorLength; i++) // put line...
+					{	IBIO.out("-");   // ...under the "1"
+					}
 					IBIO.output(" = " + negativePower);  // add the "=" and go to next line
 					IBIO.output(power);
 
 				}		// end if --> exponent is negative.
-
-				if (exponent >= 0)// this could also be: if(exponent >= 0)
+				else	// this could also be: if(exponent >= 0)
 				{
 					for(int i = 0; i < exponent; i++)
 					{	power = power * base;
